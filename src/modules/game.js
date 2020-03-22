2e3a9ec94ae0ffae2c80fe8a9e607c53420de32c
Taegyu Kim <dev.tgkim@gmail.com>
2020-03-22 13:47:53 +0900

    game 리덕스 모듈 작성

---
+++ src/modules/game.js
@@ -0,0 +1,105 @@
+// action type
+const GAME_START = "GAME_START";
+const GAME_STOP = "GAME_STOP";
+const GAME_RESTART = "GAME_RESTART";
+const GAME_QUIT = "GAME_QUIT";
+
+const INCREASE_SET = "INCREASE_SET";
+
+const PICK_CARD = "PICK_CARD";
+
+const UPDATE_SCORE = "UPDATE_SCORE";
+
+// action creator
+export const startGame = () => ({ type: GAME_START });
+
+export const stopGame = () => ({ type: GAME_STOP });
+
+export const restartGame = () => ({ type: GAME_RESTART });
+
+export const quitGame = () => ({ type: GAME_QUIT });
+
+export const increaseSet = input => ({ type: INCREASE_SET, input });
+
+export const pickCard = input => ({ type: PICK_CARD, input });
+
+export const updateScore = input => ({ type: UPDATE_SCORE, input });
+
+// initial state
+const initialState = {
+  isRunning: false,
+  currentSet: 0,
+  score: [
+    { set: 1, p1: 0, p2: 0 },
+    { set: 2, p1: 0, p2: 0 },
+    { set: 3, p1: 0, p2: 0 }
+  ],
+  pick_p1: "",
+  pick_p2: ""
+};
+
+// reducer
+const gameReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GAME_START: {
+      return {
+        ...state,
+        isRunning: true
+      };
+    }
+    case GAME_STOP: {
+      return {
+        ...state,
+        isRunning: false
+      };
+    }
+    case GAME_RESTART: {
+      return {
+        isRunning: true,
+        currentSet: 0,
+        score: [
+          { set: 1, p1: 0, p2: 0 },
+          { set: 2, p1: 0, p2: 0 },
+          { set: 3, p1: 0, p2: 0 }
+        ]
+      };
+    }
+    case GAME_QUIT: {
+      return {
+        isRunning: false,
+        currentSet: 0,
+        score: [
+          { set: 1, p1: 0, p2: 0 },
+          { set: 2, p1: 0, p2: 0 },
+          { set: 3, p1: 0, p2: 0 }
+        ]
+      };
+    }
+    case INCREASE_SET: {
+      return {
+        ...state,
+        currentSet: state.currentSet + 1
+      };
+    }
+    case PICK_CARD: {
+      return {
+        ...state
+      };
+    }
+    case UPDATE_SCORE: {
+      return {
+        ...state,
+        score: state.score.map(setScore =>
+          setScore.set === action.input.set
+            ? setScore[action.input.player]++
+            : setScore
+        )
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default gameReducer;
