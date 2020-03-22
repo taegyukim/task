d8b0cebae7c3c025b4ec47d97d45a1aa54128549
Taegyu Kim <dev.tgkim@gmail.com>
2020-03-22 11:12:09 +0900

    게임 룰 설명 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>게임 설명</h2>
+      <p>- 컴퓨터와 플레이하는 가위바위보 게임입니다.</p>
+      <p>
+        - 시작 버튼을 누르면 게임이 시작되며, 그만하기를 누르면 게임이 중단되고
+        초기화됩니다.
+      </p>
+      <p>
+        - 각 게임의 제한시간은 15초이며, 플레이어는 제한시간 내에 패를 골라야
+        합니다.
+      </p>
+      <p>
+        - 각 세트는 한 플레이어가 3승을 먼저 달성할 시 해당 플레이어의 승리로
+        처리됩니다.
+      </p>
+      <p>
+        - 한 플레이어가 3세트를 먼저 승리할 시 해당 플레이어가 최종 승자가 되며
+        게임이 종료됩니다.
+      </p>
     </div>
   );
 }
