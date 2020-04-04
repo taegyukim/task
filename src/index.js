abc3c5a8b4d7e76d9771542e7c17e084ba6628b8
Taegyu Kim <dev.tgkim@gmail.com>
2020-04-04 20:11:35 +0900

    redux-logger, redux-thunk index.js 에 적용

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./modules";
 
 import { Provider } from "react-redux";
 
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, composeWithDevTools());
+import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
+
+const logger = createLogger();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
